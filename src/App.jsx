25b557cc05c140c92eb3527fac43689590359fb1
jCasia <casjay70@gmail.com>
2023-02-28 22:34:15 +0000

    Added responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@ import "./App.scss";
 import Cards from "./components/Card";
 
 function App() {
+  const headContent = {
+    headingLight: "Reliable, effecient delivery",
+    headingBold: "Powered by Technology",
+    headingText:
+      "Our artificial Intelligence powered tools use millions of project data points to ensure that your project is successful",
+  };
+
   return (
     <main className="main">
       <div className="main-container">
         <div className="main-heading-text-container">
           <h1 className="main__heading">
             <span className="main__heading-light">
-              Reliable, effecient delivery
+              {headContent.headingLight}
             </span>
-            <span className="main__heading-bold">Powered by Technology</span>
+            <span className="main__heading-bold">
+              {headContent.headingBold}
+            </span>
           </h1>
-          <p className="main__text">
-            Our artificial Intelligence powered tools use millions of project
-            data points to ensure that your project is successful
-          </p>
+          <p className="main__text">{headContent.headingText}</p>
         </div>
         <Cards />
       </div>
